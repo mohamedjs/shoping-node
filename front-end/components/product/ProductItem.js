322f8b02c37c2dc94fa95448a41de0e52981843b
mohamedjs <mohammed_hs55@yahoo.com>
2023-03-31 09:10:53 +0200

    handle category from bakc side and front

--- front-end/components/product/ProductItem.js
+++ front-end/components/product/ProductItem.js
@@ -15,10 +15,10 @@ export const ProductItem = ({ product }) => {
         <div className='flex flex-col justify-center items-center p-5'>
             <Link href={`/product/${product.id}`}>
                 <a>
-                    <h2 className='text-lg'>{product.name}</h2>
+                    <h2 className='text-lg'>{product.brand}</h2>
                 </a>
             </Link>
-            <p className='mb-2'>{product.name}</p>
+            <p className='mb-2'>{product.stock}</p>
             <p>${product.price}</p>
             <button className='button-primary bg-gradient-to-r from-violet-500 to-fuchsia-500' type='button'>
                 Add to Cart
